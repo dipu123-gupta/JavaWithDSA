42384402ea1978e425f9017fe20b974d7c0b6320
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-02 23:05:26 +0530

    jha

--- basic/second.java
+++ basic/second.java
@@ -3,5 +3,6 @@ package basic;
 public class second {
     public static void main(String[] args) {
         System.out.println("This is the second Java file.");
+        System.out.println("Learning Java is fun!");
     }
 }
