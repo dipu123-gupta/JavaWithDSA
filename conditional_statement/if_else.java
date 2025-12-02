d29389036542ecdb961c2470816a1e706d66c3dc
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 04:44:15 +0530

    conditional statement

---
+++ conditional_statement/if_else.java
@@ -0,0 +1,12 @@
+package conditional_statement;
+
+public class if_else {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
