65042d66271bb41d099b089289ca22cf6795a7b0
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 05:23:26 +0530

    print largest of three number

--- conditional_statement/print_largest_of_3_number.java
+++ conditional_statement/print_largest_of_3_number.java
@@ -3,13 +3,13 @@ package conditional_statement;
 public class print_largest_of_3_number {
 
     public static void main(String[] args) {
-        int num1 = 10;
+        int num1 = 30;
         int num2 = 20;
-        int num3 = 15;
+        int num3 = 45;
 
         if (num1 >= num2 && num1 >= num3) {
             System.out.println("Number 1 is the largest: " + num1);
-        } else if (num2 >= num1 && num2 >= num3) {
+        } else if (num2 >= num3) {
             System.out.println("Number 2 is the largest: " + num2);
         } else {
             System.out.println("Number 3 is the largest: " + num3);
