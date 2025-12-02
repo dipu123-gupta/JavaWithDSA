304b152797be9d0c622b080ace0226cb4350ce4e
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 04:55:25 +0530

    check number is even or oddr

--- conditional_statement/print_largest.java
+++ conditional_statement/print_largest.java
@@ -3,6 +3,7 @@ package conditional_statement;
 import java.util.Scanner;
 
 public class print_largest {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
@@ -10,13 +11,13 @@ public class print_largest {
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
-        if (num1>num2) {
+        if (num1 > num2) {
             System.out.println("number 1 is largest: " + num1);
 
-        }else {
+        } else {
             System.out.println("number 2 is largest: " + num2);
         }
 
     }
-    
+
 }
