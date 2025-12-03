d03ad2dcde0d52dff8e567cba95ddb6872233368
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 08:21:39 +0530

    while loop basic loop

---
+++ loops/while_loop.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 100) {
+            System.out.println("hello world"+" "+ count);
+            count++;
+        }
+    }
+    
+}
