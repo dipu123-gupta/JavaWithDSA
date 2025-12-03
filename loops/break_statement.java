5ea3ddb30d9647d805439213430307c6edbf084d
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 09:30:59 +0530

    continue statement

---
+++ loops/break_statement.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class break_statement {
+    public static void main(String[] args) {
+        for(int i =1;i<=20;i++){
+            if(i==10){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ loops/continue_statement.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class continue_statement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue; // Skip the iteration when i is 5
+            }
+            System.out.println(i);
+        }
+    }
+}
