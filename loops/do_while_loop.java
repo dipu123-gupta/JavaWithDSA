67d3782ce876f2dbc54a41c37fd50accb8efdf88
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 09:22:39 +0530

    updated

--- loops/do_while_loop.java
+++ loops/do_while_loop.java
@@ -2,9 +2,12 @@ package loops;
 
 public class do_while_loop {
     public static void main(String[] args) {
+        // print hello world 1 to 100 using do while loop
         int num =1;
+        // do while loop
         do { 
             System.out.println("hello world"+" "+num);
+            // increment num
             num++;
         } while (num<=100);
     }
