a74a09695df4d20c6f8472a054a7687830c2d86b
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 09:20:46 +0530

    do while loop

---
+++ loops/do_while_loop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class do_while_loop {
+    public static void main(String[] args) {
+        int num =1;
+        do { 
+            System.out.println("hello world"+" "+num);
+            num++;
+        } while (num<=100);
+    }
+}
