b3f3e418b8ebf86bc62039e13f142e457dcbb115
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-03 08:54:14 +0530

    print square pattern using for loop

---
+++ loops/print_Square_pattern.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class print_Square_pattern {
+    public static void main(String[] args) {
+        
+        for (int i =1;i<=4;i++) {
+            System.out.println("****");
+        }
+    }
+}
--- loops/print_sum_of_first_n_natural_number.java
+++ loops/print_sum_of_first_n_natural_number.java
@@ -10,15 +10,32 @@ public class print_sum_of_first_n_natural_number {
     // The program then outputs the calculated sum.
     // Time Complexity: O(n)
     public static void main(String[] args) {
+
+        // Create a Scanner object to read input
         Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter a number n
         System.out.print("Enter a number n: ");
+
+        // Read the integer input
         int n = sc.nextInt();
+
+        // Initialize sum to 0
         int sum = 0;
+
+        // Initialize num to 1
         int num = 1;
+
+        // Use a while loop to calculate the sum of the first n natural numbers
         while (num <= n) {
+
+            // Add num to sum
             sum += num;
+            
+            // Increment num by 1
             num++;
         }
+        // Print the result
         System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
     }
 }
