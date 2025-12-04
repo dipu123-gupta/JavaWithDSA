e57707170fde9d99b415ee14c4643eb39469356d
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-04 08:43:32 +0530

    left trangle pattern

---
+++ pattern/pattern1.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            // Spaces
+            for (int j = 1; j < 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
