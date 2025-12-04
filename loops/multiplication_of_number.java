5ca4ee75919824bfb1c6bbc7beca7dc2a7aae4e7
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-04 06:52:42 +0530

    multiplication table

---
+++ loops/multiplication_of_number.java
@@ -0,0 +1,15 @@
+package loops;
+import java.util.*;
+
+public class multiplication_of_number {
+    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to print its multiplication table:");
+        int num = sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
