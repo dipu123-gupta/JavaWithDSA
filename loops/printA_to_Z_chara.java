fda5e79be38bcf7e7ab0c973e58762825fdeb633
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-07 20:42:46 +0530

    print A to Z character

---
+++ loops/printA_to_Z_chara.java
@@ -0,0 +1,14 @@
+package  loops;
+
+public class printA_to_Z_chara {
+    public static void main(String[] args) {
+        
+        char ch='A';
+        for (int i = 1; i <=26; i++) {
+            System.out.println(ch);
+            ch++;
+            
+        }
+
+    }
+}
